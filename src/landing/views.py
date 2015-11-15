04d4f47d9940bf3f88d76d003bce40dc965966af
Oleg Pshenichniy <olegpshenichniy@gmail.com>
2015-11-15 18:41:20 +0100

    APIAAS-landing Required libs were added. Created templates hierarchy. Fake index page.

--- src/landing/views.py
+++ src/landing/views.py
@@ -2,7 +2,16 @@ from django.views.generic import TemplateView
 
 
 class MainPage(TemplateView):
-    template_name = 'main.html'
+    template_name = 'landing/main.html'
+
+    # later we'll move this to the base class, it's ok for now
+    def get_context_data(self, **kwargs):
+        context = super(MainPage, self).get_context_data(**kwargs)
+        context['page_title'] = u'APIAAS | Api as a service'
+        context['page_header'] = u'Welcome'
+        context['page_header_desc'] = u'Base GAE project with Django Rest Framework'
+        
+        return context
 
 
 main_page = MainPage.as_view()
